965f92693c967ccd68dc2db9fd2ceb9ec23a68cc
Lucy Ivatt <li571@york.ac.uk>
2023-01-11 20:43:49 +0000

    updated q3 weights and added classify symbols code tile

--- classify_symbols.py
+++ classify_symbols.py
@@ -28,8 +28,8 @@ class SymbolClassifier(nn.Module):
             nn.MaxPool2d(kernel_size=(2, 2), stride=2),
             nn.Dropout(0.2),
         )
-        self.MLP = nn.Sequential(
 
+        self.MLP = nn.Sequential(
             nn.Linear(in_features=12*12*24, out_features=300),
             nn.BatchNorm1d(300),
             nn.ReLU(),
@@ -37,39 +37,34 @@ class SymbolClassifier(nn.Module):
 
             nn.Linear(in_features=300, out_features=180),
             nn.BatchNorm1d(180),
+            nn.ReLU(),
+            nn.Dropout(0.2),
 
-            # initialize our softmax classifier
             nn.Linear(in_features=180, out_features=5),
-            nn.LogSoftmax(dim=1)
+            nn.LogSoftmax(dim=1)  # probability distribution
         )
 
     def forward(self, x):
-        # Input x has dimensions B x 1 x 48 x 48, B is batch size
+        # Input x has dimensions 64 x 1 x 48 x 48
         x = self.ConvolutionalLayers(x)
         x = x.view(x.size(0), -1)
-        x = self.MLP(x)
-        # Output has dimensions B x 5
+        x = self.MLP(x)  # 64 x 5
         return x
 
 
 def classify(images):
-    # Determine which device the input tensor is on
     device = torch.device("cuda" if images.is_cuda else "cpu")
-
     model = SymbolClassifier()
-    # Move to same device as input images
     model = model.to(device)
+
     # Load network weights
     model.load_state_dict(torch.load(
         'weights.pkl', map_location=torch.device(device)))
+
     # Put model in evaluation mode
     model.eval()
 
-    # Optional: do whatever preprocessing you do on the images
-    # if not included as tranformations inside the model
-
     with torch.no_grad():
-        # Pass images to model, get back logits or probabilities
         output = model(images)
 
     # Select class with highest probability for each input
@@ -77,19 +72,3 @@ def classify(images):
 
     # Return predicted classes
     return predicted_classes
-
-
-image_tensors = []
-transform = transforms.Compose(
-    [transforms.Grayscale(num_output_channels=1), transforms.ToTensor()])
-
-for i in ["0000", "0001", "0002", "0003"]:
-    img = Image.open("images/classify_testing/" + i + ".tif")
-    img = transform(img)
-    img = torch.unsqueeze(img, 0)
-    image_tensors.append(img)
-
-cat = torch.cat(image_tensors)
-
-output = classify(cat)
-print(output)
